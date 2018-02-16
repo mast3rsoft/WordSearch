fe9d03dc0fbca67a8396fa1924f7e6196d1349dd
mast3rsoft <ceo@mast3rsoft.com>
2018-02-16 09:46:53 +0100

    Updated Model to include searching the answers for the game

--- WordSearch.py
+++ WordSearch.py
@@ -1,7 +1,15 @@
-
-
 import random
 class WordSearch():
+    HORIZONTAL = 0
+    VERTICAL = 1
+    DIAGONAL = 2
+    REVHORIZONTAL = 3
+    REVVERTICAL = 4
+    REVDIAGONAL = 5
+    REVFLIPDIAGONA = 6
+    FLIPDIAGONAL = 7
+    DONTCARE = -100
+    wordPosition = {}
     def __init__(self, searchWords, maxX = 20, maxY = 20):
         self.maxX = maxX
         self.maxY = maxY
@@ -17,20 +25,10 @@ class WordSearch():
                 self.grid[row].append('*')
         for word in searchWords:
             DIR = random.randint(0, 7)
-        while not self.engrave(word, self.DONTCARE , self.DONTCARE , DIR):
-            pass
+            while not self.engrave(word, self.DONTCARE , self.DONTCARE , DIR):
+                pass
         self.obfusticate()
-                   
-    HORIZONTAL = 0
-    VERTICAL = 1
-    DIAGONAL = 2
-    REVHORIZONTAL = 3
-    REVVERTICAL = 4
-    REVDIAGONAL = 5
-    REVFLIPDIAGONA = 6
-    FLIPDIAGONAL = 7
-    DONTCARE = -100
-    def engrave(self,word, x, y, direction):
+    def engrave(self, word, x, y, direction):
         if len(word) == 0:
             return True
         # word has length > 0
@@ -83,16 +81,130 @@ class WordSearch():
             y = undoy
             x = undox
             self.grid[y][x] = undovalue
-            return False
-        
+            return False       
     def obfusticate(self):
         for row in self.grid:
             for i in range(len(row)):
                 if row[i] == '*':
                     row[i] = 'abcdefghijklmnopqrstuvwxyz0123456789'[random.randint(0,25)]
-                
-
-
     def letter(self,x,y):
         return self.grid[x][y]
-    
+    def findWords(self, words):
+        for word in words:
+            firstLetter = word[0]
+            positions = None
+            y = 0; found = False
+            while y < self.maxY and not found:
+                x = 0 
+                while x < self.maxX and not found:
+                    if firstLetter == self.grid[y][x]:
+                        positions  = self.wordIsHere(word, x, y)
+                        if positions:
+                            found = True
+                            break
+                    x += 1
+                if not found:
+                    y += 1
+            if found:
+                self.wordPosition[word] = positions
+    def wordIsHere(self,word, firstX, firstY):
+         maxX = self.maxX
+         maxY = self.maxY
+         # horizontal
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if x == maxX or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             x += 1
+         if found:
+             return positions
+         # vertical
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if y == maxY or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             y += 1
+         if found:
+             return positions        
+         # reverse horizontal
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if x == -1 or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             x -= 1
+         if found:
+             return positions
+         # reverse vertical
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if y == -1 or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             y -= 1
+         if found:
+             return positions         
+         # diagonal
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if y == maxY or x == maxX or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             x += 1
+             y += 1
+         if found:
+             return positions 
+         # reverse diagonal              
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if y == -1 or x == -1 or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             x -= 1
+             y -= 1
+         if found:
+             return positions 
+         # flip diagonal
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if y == -1 or x == maxX or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             x += 1
+             y -= 1
+         if found:
+             return positions
+         # reverse flip diagonal
+         found = True; x = firstX; y = firstY; positions = []
+         for letter in word:
+             if y == maxY or x == -1 or letter != self.grid[y][x]:
+                 found = False
+                 break
+             positions.append((y, x))
+             x -= 1
+             y += 1
+         if found:
+             return positions
+         #
+         return None  
+# Test Progamm for WordSearch Genertator        
+if __name__ == '__main__':
+    words = ("Gugu,Gaga")
+    w = WordSearch(words, 5, 5)
+    def printGrid(grid):
+        for row in grid:
+            for column in row:
+                print("%s" % column, end='')
+            print()
+    printGrid(w.grid)
+    w.findWords(words.split(','))
+    print(w.wordPosition)
